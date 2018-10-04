13f010abc5a2703e02acbb982e724b9c8415ebb9
Madhu Rajanna <mrajanna@redhat.com>
2018-10-04 15:26:14 +0530

    use Capacity from volume response
    Earlier there was not capacity field
    in volume response, when we get the volume
    list from glusterd2.
    
    recently in GD2 we are supporting
    volume capacity field in volume response
    
    Signed-off-by: Madhu Rajanna <mrajanna@redhat.com>

--- pkg/glusterfs/driver_test.go
+++ pkg/glusterfs/driver_test.go
@@ -20,7 +20,6 @@ import (
 
 var volumeCache = make(map[string]uint64)
 
-// nolint: gocyclo
 func TestDriverSuite(t *testing.T) {
 	glusterMounter = &mount.FakeMounter{}
 	socket := "/tmp/csi.sock"
@@ -35,81 +34,10 @@ func TestDriverSuite(t *testing.T) {
 	}
 	defer os.Remove(socket)
 
-	id := uuid.Parse("02dfdd19-e01e-46ec-a887-97b309a7dd2f")
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
 		switch r.Method {
 		case "GET":
-			if strings.Contains(r.URL.String(), "/v1/peers") {
-				resp := make(api.PeerListResp, 1)
-				resp[0] = api.PeerGetResp{
-					Name: "node1.com",
-					PeerAddresses: []string{
-						"127.0.0.1:24008"},
-					ClientAddresses: []string{
-						"127.0.0.1:24007",
-						"127.0.0.1:24007"},
-					Online: true,
-					PID:    24935,
-					Metadata: map[string]string{
-						"_zone": "02dfdd19-e01e-46ec-a887-97b309a7dd2f",
-					},
-				}
-				resp = append(resp, api.PeerGetResp{
-					Name: "node2.com",
-					PeerAddresses: []string{
-						"127.0.0.1:24008"},
-					ClientAddresses: []string{
-						"127.0.0.1:24007"},
-					Online: true,
-					PID:    24935,
-					Metadata: map[string]string{
-						"_zone": "02dfdd19-e01e-46ec-a887-97b309a7dd2f",
-					},
-				})
-				writeResp(w, http.StatusOK, resp, t)
-				return
-			}
-
-			if strings.HasSuffix(r.URL.String(), "/v1/volumes") {
-				resp := make(api.VolumeListResp, 1)
-				resp[0] = api.VolumeGetResp{
-					ID:       id,
-					Name:     "test1",
-					Metadata: map[string]string{volumeOwnerAnn: glusterfsCSIDriverName},
-				}
-
-				resp = append(resp, api.VolumeGetResp{
-					ID:       id,
-					Name:     "test1",
-					Metadata: map[string]string{volumeOwnerAnn: glusterfsCSIDriverName},
-				})
-				writeResp(w, http.StatusOK, resp, t)
-				volumeCache["test1"] = 1000
-				return
-			}
-
-			vol := strings.Split(strings.Trim(r.URL.String(), "/"), "/")
-			if checkVolume(vol[2]) {
-				resp := api.VolumeStatusResp{
-					Info: api.VolumeInfo{
-						ID:       id,
-						Name:     vol[2],
-						Metadata: map[string]string{volumeOwnerAnn: glusterfsCSIDriverName},
-					},
-					Online: true,
-					Size: api.SizeInfo{
-						Capacity: volumeCache[vol[2]],
-					},
-				}
-				writeResp(w, http.StatusOK, resp, t)
-				return
-			}
-			resp := api.ErrorResp{}
-			resp.Errors = append(resp.Errors, api.HTTPError{
-				Code: 1,
-			})
-			writeResp(w, http.StatusNotFound, resp, t)
+			handleGETRequest(w, r, t)
 			return
 
 		case "DELETE":
@@ -117,21 +45,8 @@ func TestDriverSuite(t *testing.T) {
 			return
 
 		case "POST":
-			if strings.HasSuffix(r.URL.String(), "start") || strings.HasSuffix(r.URL.String(), "stop") {
-				w.WriteHeader(http.StatusOK)
-				return
-			}
-
-			if strings.HasPrefix(r.URL.String(), "/v1/volumes") {
-				var resp api.VolumeCreateResp
-
-				var req api.VolCreateReq
-				defer r.Body.Close()
-				json.NewDecoder(r.Body).Decode(&req)
-				resp.Name = req.Name
-				volumeCache[req.Name] = req.Size
-				writeResp(w, http.StatusCreated, resp, t)
-			}
+			handlePOSTRequest(w, r, t)
+			return
 		}
 	}))
 
@@ -167,6 +82,93 @@ func TestDriverSuite(t *testing.T) {
 	}
 
 	sanity.Test(t, cfg)
+}
+
+func handleGETRequest(w http.ResponseWriter, r *http.Request, t *testing.T) {
+	id := uuid.Parse("02dfdd19-e01e-46ec-a887-97b309a7dd2f")
+	if strings.Contains(r.URL.String(), "/v1/peers") {
+		resp := make(api.PeerListResp, 1)
+		resp[0] = api.PeerGetResp{
+			Name: "node1.gluster.org",
+			PeerAddresses: []string{
+				"127.0.0.1:24008"},
+			ClientAddresses: []string{
+				"127.0.0.1:24007",
+				"127.0.0.1:24007"},
+			Online: true,
+			PID:    24935,
+			Metadata: map[string]string{
+				"_zone": "02dfdd19-e01e-46ec-a887-97b309a7dd2f",
+			},
+		}
+		resp = append(resp, api.PeerGetResp{
+			Name: "node2.com",
+			PeerAddresses: []string{
+				"127.0.0.1:24008"},
+			ClientAddresses: []string{
+				"127.0.0.1:24007"},
+			Online: true,
+			PID:    24935,
+			Metadata: map[string]string{
+				"_zone": "02dfdd19-e01e-46ec-a887-97b309a7dd2f",
+			},
+		})
+		writeResp(w, http.StatusOK, resp, t)
+		return
+	}
+
+	if strings.HasSuffix(r.URL.String(), "/v1/volumes") {
+		resp := make(api.VolumeListResp, 1)
+		resp[0] = api.VolumeGetResp{
+			ID:       id,
+			Name:     "test1",
+			Metadata: map[string]string{volumeOwnerAnn: glusterfsCSIDriverName},
+			Capacity: 1000,
+		}
+		writeResp(w, http.StatusOK, resp, t)
+		volumeCache["test1"] = 1000
+		return
+	}
+
+	vol := strings.Split(strings.Trim(r.URL.String(), "/"), "/")
+	if checkVolume(vol[2]) {
+		resp := api.VolumeStatusResp{
+			Info: api.VolumeInfo{
+				ID:       id,
+				Name:     vol[2],
+				Metadata: map[string]string{volumeOwnerAnn: glusterfsCSIDriverName},
+			},
+			Online: true,
+			Size: api.SizeInfo{
+				Capacity: volumeCache[vol[2]],
+			},
+		}
+		writeResp(w, http.StatusOK, resp, t)
+		return
+	}
+	resp := api.ErrorResp{}
+	resp.Errors = append(resp.Errors, api.HTTPError{
+		Code: 1,
+	})
+	writeResp(w, http.StatusNotFound, resp, t)
+}
+
+func handlePOSTRequest(w http.ResponseWriter, r *http.Request, t *testing.T) {
+	if strings.HasSuffix(r.URL.String(), "start") || strings.HasSuffix(r.URL.String(), "stop") {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	if strings.HasPrefix(r.URL.String(), "/v1/volumes") {
+		var resp api.VolumeCreateResp
+
+		var req api.VolCreateReq
+		defer r.Body.Close()
+		json.NewDecoder(r.Body).Decode(&req)
+		resp.Name = req.Name
+		volumeCache[req.Name] = req.Size
+		writeResp(w, http.StatusCreated, resp, t)
+	}
 }
 
 func checkVolume(vol string) bool {
