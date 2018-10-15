101a4302937ea8aa1149cf87edbed6f45b9812db
Humble Chirammal <hchiramm@redhat.com>
2018-10-15 17:36:25 +0530

    Add markdown rules url for reference.
    Signed-off-by: Humble Chirammal <hchiramm@redhat.com>

--- scripts/mdl-style.rb
+++ scripts/mdl-style.rb
@@ -1,5 +1,8 @@
 all
 
+#Refer below url for more information about the markdown rules.
+#https://github.com/markdownlint/markdownlint/blob/master/docs/RULES.md
+
 rule 'MD013', :code_blocks => false, :tables => false
 
 exclude_rule 'MD040' # Fenced code blocks should have a language specified
