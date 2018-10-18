d9c3344e38991778859bcc6651f23b83fdc29cf7
John Strunk <jstrunk@redhat.com>
2018-10-18 14:36:33 -0400

    Move CentOS CI to containerized build
    Signed-off-by: John Strunk <jstrunk@redhat.com>

--- build.sh
+++ build.sh
@@ -17,4 +17,18 @@ $DOCKER_CMD build \
         --build-arg version="$VERSION" \
         --build-arg builddate="$BUILDDATE" \
         -f pkg/glusterfs/Dockerfile \
-        .
+        . \
+|| exit 1
+
+if [ "$RUN_TESTS" -ne 0 ]; then
+        rm -f profile.cov
+        $DOCKER_CMD build \
+                -t glusterfs-csi-driver-build \
+                --target build \
+                --build-arg RUN_TESTS="$RUN_TESTS" \
+                -f pkg/glusterfs/Dockerfile \
+                . \
+        && \
+        $DOCKER_CMD run --rm glusterfs-csi-driver-build \
+                cat /profile.cov > profile.cov
+fi
--- extras/centos-ci.sh
+++ extras/centos-ci.sh
@@ -5,31 +5,12 @@
 # if anything fails, we'll abort
 set -e
 
-REQ_GO_VERSION='1.10.2'
-# install Go
-if ! yum -y install "golang >= $REQ_GO_VERSION"
-then
-	# not the right version, install manually
-	# download URL comes from https://golang.org/dl/
-	curl -O https://storage.googleapis.com/golang/go${REQ_GO_VERSION}.linux-amd64.tar.gz
-	tar xzf go${REQ_GO_VERSION}.linux-amd64.tar.gz -C /usr/local
-	export PATH=$PATH:/usr/local/go/bin
-fi
+# Install buildah & Docker
+yum-config-manager \
+        --add-repo \
+        https://download.docker.com/linux/centos/docker-ce.repo
+yum -y install buildah docker-ce
+systemctl start docker
 
-# also needs git, hg, bzr, svn gcc and make
-yum -y install git mercurial bzr subversion gcc make
-
-export CSISRC=$GOPATH/src/github.com/gluster/gluster-csi-driver
-cd "$CSISRC"
-
-# install the build and test requirements
-./scripts/install-reqs.sh
-
-# install vendored dependencies
-make vendor-install
-
-# verify build
-make glusterfs-csi-driver
-
-# run tests
-make test TESTOPTIONS=-v
+# Build containers
+./build.sh
