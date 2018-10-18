828d7975b1cd0116910ef666b3c2c96d5f78e91c
John Strunk <jstrunk@redhat.com>
2018-10-18 12:31:01 -0400

    Moves all tests into build container
    With this commit, all building and testing are done within the
    build-stage container. This means devs don't need golang tools
    installed, and it greatly simplifies the Travis config as well.
    
    Signed-off-by: John Strunk <jstrunk@redhat.com>

--- build.sh
+++ build.sh
@@ -5,11 +5,15 @@ set -e
 # Allow overriding default docker command
 DOCKER_CMD=${DOCKER_CMD:-docker}
 
+# Allow disabling tests during build
+RUN_TESTS=${RUN_TESTS:-1}
+
 VERSION="$(git describe --dirty --always --tags | sed 's/-/./2' | sed 's/-/./2')"
 BUILDDATE="$(date -u '+%Y-%m-%dT%H:%M:%S.%NZ')"
 
 $DOCKER_CMD build \
         -t glusterfs-csi-driver \
+        --build-arg RUN_TESTS="$RUN_TESTS" \
         --build-arg version="$VERSION" \
         --build-arg builddate="$BUILDDATE" \
         -f pkg/glusterfs/Dockerfile \
