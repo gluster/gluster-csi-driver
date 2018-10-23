62d4871c4bb13145c5522a79ec4c439426c861ce
Jose A. Rivera <jarrpa@redhat.com>
2018-10-23 10:01:08 -0500

    Tag build container if running tests
    Previously, the second docker build would not actually use any images
    from the docker cache. If testing, build and tag the build container
    first, then build the final container. If not testing, only build once.
    
    Signed-off-by: Jose A. Rivera <jarrpa@redhat.com>

--- build.sh
+++ build.sh
@@ -11,6 +11,20 @@ RUN_TESTS=${RUN_TESTS:-1}
 VERSION="$(git describe --dirty --always --tags | sed 's/-/./2' | sed 's/-/./2')"
 BUILDDATE="$(date -u '+%Y-%m-%dT%H:%M:%S.%NZ')"
 
+# If running tests, create build container to extract profile data later
+if [ "$RUN_TESTS" -ne 0 ]; then
+        rm -f profile.cov
+        $DOCKER_CMD build \
+                -t glusterfs-csi-driver-build \
+                --target build \
+                --build-arg RUN_TESTS="$RUN_TESTS" \
+                --build-arg version="$VERSION" \
+                --build-arg builddate="$BUILDDATE" \
+                -f pkg/glusterfs/Dockerfile \
+                . \
+        || exit 1
+fi
+
 #-- Build final container
 $DOCKER_CMD build \
         -t glusterfs-csi-driver \
@@ -21,15 +35,8 @@ $DOCKER_CMD build \
         . \
 || exit 1
 
+# If running tests, extract profile data
 if [ "$RUN_TESTS" -ne 0 ]; then
-        rm -f profile.cov
-        $DOCKER_CMD build \
-                -t glusterfs-csi-driver-build \
-                --target build \
-                --build-arg RUN_TESTS="$RUN_TESTS" \
-                -f pkg/glusterfs/Dockerfile \
-                . \
-        && \
         $DOCKER_CMD run --rm glusterfs-csi-driver-build \
                 cat /profile.cov > profile.cov
 fi
