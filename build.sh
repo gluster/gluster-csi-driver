fde8c089a2e7f1be19fa2ade1285c3bc231f8f0b
Jose A. Rivera <jarrpa@redhat.com>
2018-10-25 12:40:08 -0500

    Add more build parameters
    Signed-off-by: Jose A. Rivera <jarrpa@redhat.com>

--- build.sh
+++ build.sh
@@ -2,11 +2,23 @@
 
 set -e
 
+# Set driver name
+DRIVER="${DRIVER:-glusterfs-csi-driver}"
+
+# Set which docker repo to tag
+REPO="${REPO:-gluster/}"
+
+# Base image to use for final container images
+FINAL_BASE="${FINAL_BASE:-centos:7.5.1804}"
+
 # Allow overriding default docker command
 DOCKER_CMD=${DOCKER_CMD:-docker}
 
 # Allow disabling tests during build
 RUN_TESTS=${RUN_TESTS:-1}
+
+# Cleanup build context when done
+RM_BUILD=${RM_BUILD:-1}
 
 VERSION="$(git describe --dirty --always --tags | sed 's/-/./2' | sed 's/-/./2')"
 BUILDDATE="$(date -u '+%Y-%m-%dT%H:%M:%S.%NZ')"
@@ -20,6 +32,7 @@ build_args+=( --build-arg "RUN_TESTS=$RUN_TESTS" )
 build_args+=( --build-arg "GO_DEP_VERSION=$GO_DEP_VERSION" )
 build_args+=( --build-arg "GO_METALINTER_VERSION=$GO_METALINTER_VERSION" )
 build_args+=( --build-arg "GO_METALINTER_THREADS=$GO_METALINTER_THREADS" )
+build_args+=( --build-arg "FINAL_BASE=$FINAL_BASE" )
 build_args+=( --build-arg "version=$VERSION" )
 build_args+=( --build-arg "builddate=$BUILDDATE" )
 
@@ -38,7 +51,7 @@ $DOCKER_CMD build \
 
 #-- Build final container
 $DOCKER_CMD build \
-        -t glusterfs-csi-driver \
+        -t "${REPO}${DRIVER}" \
         --target driver \
         "${build_args[@]}" \
         -f pkg/glusterfs/Dockerfile \
@@ -52,4 +65,6 @@ if [ "$RUN_TESTS" -ne 0 ]; then
                 cat /profile.cov > profile.cov
 fi
 
-$DOCKER_CMD rmi glusterfs-csi-driver-build
+if [ "$RM_BUILD" -ne 0 ]; then
+        $DOCKER_CMD rmi -f glusterfs-csi-driver-build
+fi
