7c95d9eee4bcbd90b1090b4cd01dea982d58b088
Jose A. Rivera <jarrpa@redhat.com>
2018-10-25 12:39:39 -0500

    Tweak build logic / labeling
    Signed-off-by: Jose A. Rivera <jarrpa@redhat.com>

--- build.sh
+++ build.sh
@@ -15,42 +15,41 @@ GO_DEP_VERSION="${GO_DEP_VERSION}"
 GO_METALINTER_VERSION="${GO_METALINTER_VERSION:-v2.0.11}"
 GO_METALINTER_THREADS=${GO_METALINTER_THREADS:-4}
 
+build_args=()
+build_args+=( --build-arg "RUN_TESTS=$RUN_TESTS" )
+build_args+=( --build-arg "GO_DEP_VERSION=$GO_DEP_VERSION" )
+build_args+=( --build-arg "GO_METALINTER_VERSION=$GO_METALINTER_VERSION" )
+build_args+=( --build-arg "GO_METALINTER_THREADS=$GO_METALINTER_THREADS" )
+build_args+=( --build-arg "version=$VERSION" )
+build_args+=( --build-arg "builddate=$BUILDDATE" )
+
 # Print Docker version
 echo "=== Docker Version ==="
 $DOCKER_CMD version
 
-# If running tests, create build container to extract profile data later
-if [ "$RUN_TESTS" -ne 0 ]; then
-        rm -f profile.cov
-        $DOCKER_CMD build \
-                -t glusterfs-csi-driver-build \
-                --target build \
-                --build-arg RUN_TESTS="$RUN_TESTS" \
-                --build-arg GO_DEP_VERSION="$GO_DEP_VERSION" \
-                --build-arg GO_METALINTER_VERSION="$GO_METALINTER_VERSION" \
-                --build-arg GO_METALINTER_THREADS="$GO_METALINTER_THREADS" \
-                --build-arg version="$VERSION" \
-                --build-arg builddate="$BUILDDATE" \
-                -f pkg/glusterfs/Dockerfile \
-                . \
-        || exit 1
-fi
+# Run container build
+$DOCKER_CMD build \
+        -t glusterfs-csi-driver-build \
+        --target build \
+        "${build_args[@]}" \
+        -f pkg/glusterfs/Dockerfile \
+        . \
+|| exit 1
 
 #-- Build final container
 $DOCKER_CMD build \
         -t glusterfs-csi-driver \
-        --build-arg RUN_TESTS="$RUN_TESTS" \
-        --build-arg GO_DEP_VERSION="$GO_DEP_VERSION" \
-        --build-arg GO_METALINTER_VERSION="$GO_METALINTER_VERSION" \
-        --build-arg GO_METALINTER_THREADS="$GO_METALINTER_THREADS" \
-        --build-arg version="$VERSION" \
-        --build-arg builddate="$BUILDDATE" \
+        --target driver \
+        "${build_args[@]}" \
         -f pkg/glusterfs/Dockerfile \
         . \
 || exit 1
 
 # If running tests, extract profile data
 if [ "$RUN_TESTS" -ne 0 ]; then
-        $DOCKER_CMD run --rm glusterfs-csi-driver-build \
+        rm -f profile.cov
+        $DOCKER_CMD run glusterfs-csi-driver-build \
                 cat /profile.cov > profile.cov
 fi
+
+$DOCKER_CMD rmi glusterfs-csi-driver-build
