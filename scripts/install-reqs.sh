9aea5b4a9b8f48765b0c8d66196da08396b91c77
Madhu Rajanna <mrajanna@redhat.com>
2019-01-06 10:53:30 +0530

    update gometalinter to 2.0.12
    Signed-off-by: Madhu Rajanna <mrajanna@redhat.com>

--- scripts/install-reqs.sh
+++ scripts/install-reqs.sh
@@ -4,37 +4,37 @@ GOPATH=$(go env GOPATH)
 GOBINDIR=$GOPATH/bin
 
 install_dep() {
-  DEPVER="v0.5.0"
-  DEPURL="https://github.com/golang/dep/releases/download/${DEPVER}/dep-linux-amd64"
-  if type dep >/dev/null 2>&1; then
-    local version
-    version=$(dep version | awk '/^ version/{print $3}')
-    if [[ $version == "$DEPVER" || $version >  $DEPVER ]]; then
-      echo "dep ${DEPVER} or greater is already installed"
-      return
-    fi
-  fi
+	DEPVER="v0.5.0"
+	DEPURL="https://github.com/golang/dep/releases/download/${DEPVER}/dep-linux-amd64"
+	if type dep >/dev/null 2>&1; then
+		local version
+		version=$(dep version | awk '/^ version/{print $3}')
+		if [[ $version == "$DEPVER" || $version > $DEPVER ]]; then
+			echo "dep ${DEPVER} or greater is already installed"
+			return
+		fi
+	fi
 
-  echo "Installing dep. Version: ${DEPVER}"
-  DEPBIN=$GOPATH/bin/dep
-  curl -L -o "$DEPBIN" $DEPURL
-  chmod +x "$DEPBIN"
+	echo "Installing dep. Version: ${DEPVER}"
+	DEPBIN=$GOPATH/bin/dep
+	curl -L -o "$DEPBIN" $DEPURL
+	chmod +x "$DEPBIN"
 }
 
 install_gometalinter() {
-  LINTER_VER="2.0.11"
-  LINTER_TARBALL="gometalinter-${LINTER_VER}-linux-amd64.tar.gz"
-  LINTER_URL="https://github.com/alecthomas/gometalinter/releases/download/v${LINTER_VER}/${LINTER_TARBALL}"
+	LINTER_VER="2.0.12"
+	LINTER_TARBALL="gometalinter-${LINTER_VER}-linux-amd64.tar.gz"
+	LINTER_URL="https://github.com/alecthomas/gometalinter/releases/download/v${LINTER_VER}/${LINTER_TARBALL}"
 
-  if type gometalinter >/dev/null 2>&1; then
-    echo "gometalinter already installed"
-    return
-  fi
+	if type gometalinter >/dev/null 2>&1; then
+		echo "gometalinter already installed"
+		return
+	fi
 
-  echo "Installing gometalinter. Version: ${LINTER_VER}"
-  curl -L -o "$GOBINDIR/$LINTER_TARBALL" $LINTER_URL
-  tar -zxf "$GOBINDIR/$LINTER_TARBALL" --overwrite --strip-components 1 --exclude={COPYING,*.md} -C "$GOBINDIR"
-  rm -f "$GOBINDIR/$LINTER_TARBALL"
+	echo "Installing gometalinter. Version: ${LINTER_VER}"
+	curl -L -o "$GOBINDIR/$LINTER_TARBALL" $LINTER_URL
+	tar -zxf "$GOBINDIR/$LINTER_TARBALL" --overwrite --strip-components 1 --exclude={COPYING,*.md} -C "$GOBINDIR"
+	rm -f "$GOBINDIR/$LINTER_TARBALL"
 }
 
 install_dep
